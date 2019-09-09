72ee1d0bdebc230d01bbf01bccc61ab16d5ab3a7
Student <example@example.com>
2019-09-09 10:02:33 -0700

    It add forms and refs for submission function

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 
 function NewTicketForm(){
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) => {_names = input;}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {_location = input;}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(textarea) => {_issue = textarea;}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
