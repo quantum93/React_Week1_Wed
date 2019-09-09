c6b5bbda7153ffed512926db1101cbbf55962d6f
Student <example@example.com>
2019-09-09 10:52:33 -0700

    create dynamic ticket list so form adds tickets to home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
-import MyStyledComponent from './MyStyledComponent';
-import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
-import Error404 from './Error404';
 import NewTicketControl from './NewTicketControl';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
-function App(){
-  return (
-    <div>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket) {
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
       <Header/>
       <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketControl} />
+        <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+        <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
         <Route component={Error404} />
       </Switch>
-    </div>
-  )
+      </div>
+    );
+  }
 }
 
 export default App
